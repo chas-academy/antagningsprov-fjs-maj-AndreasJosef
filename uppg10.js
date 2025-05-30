daa0333fdb1725f6b4184aa8a1852e8e530611b3
AndreasJosef <86117013+AndreasJosef@users.noreply.github.com>
2025-05-30 11:22:56 +0200

    Solved Uppgift 10

--- uppg10.js
+++ uppg10.js
@@ -1,17 +1,36 @@
-function uppg10(){
-    const numbers = [5, 12, 7, 10, 20, 9, 11];
-  
-    // skapa tre arrayer - bigEven, smallOdd, other
-    // loopa igenom numbers och placera talen i respektive array
-    // bigEven ska innehålla tal större än 10 och jämnt delbara med 2
-    // smallOdd ska innehålla tal mindre än 10 och udda
-    // other ska innehålla alla andra tal
-    // returnera ett objekt med de tre arrayerna
+function uppg10() {
+  const numbers = [5, 12, 7, 10, 20, 9, 11];
 
-  
-    
-  
-  }
-  
-  module.exports = { uppg10 };
-  
+  // skapa tre arrayer - bigEven, smallOdd, other
+  let bigEven = [];
+  let smallOdd = [];
+  let other = [];
+
+  // loopa igenom numbers och placera talen i respektive array
+  numbers.forEach(number => {
+
+    // bigEven ska innehålla tal större än 10 och jämnt delbara med 2
+    if (number > 10 && number % 2 === 0) {
+      bigEven.push(number);
+    }
+
+    // smallOdd ska innehålla tal mindre än 10 och udda
+    else if (number < 10 && number % 2 !== 0) {
+      smallOdd.push(number)
+
+      // other ska innehålla alla andra tal
+    } else {
+      other.push(number)
+
+    }
+  });
+
+  // returnera ett objekt med de tre arrayerna
+  return {
+    bigEven,
+    smallOdd,
+    other
+  };
+}
+
+module.exports = { uppg10 };
