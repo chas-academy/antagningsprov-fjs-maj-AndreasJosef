e936db6f0db0e1a044e1092f8586ab61c1b58831
AndreasJosef <86117013+AndreasJosef@users.noreply.github.com>
2025-05-30 10:23:14 +0200

    Solved Uppgift 5

--- uppg5.js
+++ uppg5.js
@@ -3,8 +3,17 @@ function uppg5() {
     // skriv en if-sats som jämför två tal
     // om det första talet är större än det andra, skriv ut "Det första talet är större än det andra"
     // annars skriv ut "Det andra talet är större än det första"
+    let tal1, tal2;
 
+    tal1 = 12;
+    tal2 = 2;
 
+    if (tal1 > tal2) {
+        console.log("Det första talet är större än det andra");
+    }
+    else {
+        console.log("Det andra talet är större än det första");
+    }
 
 }
 
