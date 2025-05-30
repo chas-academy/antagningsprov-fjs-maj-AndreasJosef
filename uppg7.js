b5a90ae1afe8c6436274ede5d5117279d70e1239
AndreasJosef <86117013+AndreasJosef@users.noreply.github.com>
2025-05-30 10:33:13 +0200

    Solved Uppgift 7

--- uppg7.js
+++ uppg7.js
@@ -3,9 +3,14 @@
 function uppg7() {
    
     // skapa en funktion som tar in två tal som argument
-    // funktionen ska returnera summan av talen
-    // kalla på funktionen i en return statement och ge den parametrarna 5 och 10
+    function sum(x, y) {
 
+        // funktionen ska returnera summan av talen
+        return x + y;
+    }
+
+    // kalla på funktionen i en return statement och ge den parametrarna 5 och 10
+    return sum(5, 10)
  
 }
 
