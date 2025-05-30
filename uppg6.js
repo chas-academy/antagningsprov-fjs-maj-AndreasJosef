c6bd6bb4844ba06df051ae211f54f13d4ab82118
AndreasJosef <86117013+AndreasJosef@users.noreply.github.com>
2025-05-30 10:29:40 +0200

    Solved Uppgift 6

--- uppg6.js
+++ uppg6.js
@@ -3,7 +3,12 @@
 function uppg6(){
 
     // Skapa en array med namn som du döper till names
+    let names = ['Sabine', 'Peter', 'Klaus'];
+
     // Loopa igenom arrayen och skriv ut alla namn i konsolen
+    names.forEach((name) => {
+        console.log(name);
+    });
 
  
 }
